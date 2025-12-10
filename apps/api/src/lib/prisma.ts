50eca43a1fb4380ff23edae2d23423f5e5dd04c0
Uday Senapati <udaysenapati6878@gmail.com>
2025-12-10 22:02:45 +0530

    update prisma.ts

--- apps/api/src/lib/prisma.ts
+++ apps/api/src/lib/prisma.ts
@@ -1,11 +1,7 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../../generated/prisma/index.js";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma =
-    globalForPrisma.prisma ||
-    new PrismaClient({
-        log: ["query"], // optional for debugging
-    });
+export const prisma = globalForPrisma.prisma || new PrismaClient({});
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
